1b6845747fa0bc371a9c04b3ae11b4aed1989947
Steven Silvester <stslve@amazon.com>
2020-07-15 13:23:14 -0500

    run vega-statitics through babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,15 @@
 module.exports = {
-    entry: "./index.js"
+    entry: "./index.js",
+    module: {
+        rules: [{
+            test: /vega-statistics\/src\/numbers.js$/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env'],
+                    plugins: ['@babel/plugin-proposal-object-rest-spread']
+                }
+            }
+        }]
+    }
 }
